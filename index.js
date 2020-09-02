c158699c4651c4202c638f5aaec3156135ebf517
barisenak <mariya1999-7@mail.ru>
2020-09-02 18:41:22 +0300

    redo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const userName = document.getElementById("userName")
+const userAge = document.getElementById("userAge")
 const attentionString = document.getElementById("string")
 const date = document.getElementById("date")
 const woman = document.getElementById("woman")
@@ -6,12 +7,21 @@ const man = document.getElementById("man")
 const education = document.getElementById("education")
 const selectForm = document.getElementById("select")
 const modalWindow = document.getElementById("window")
+const mnemonic = document.getElementById("mnemonic")
+console.log(mnemonic);
 let gender
 
 function getGender() {
     woman.checked ? gender = 'женщина' : gender = 'мужчина'; 
     return gender
 }
+
+userAge.addEventListener("keyup", () =>{
+    if (!Number(userAge.value)){
+        userAge.style.border = "2px solid red"
+    } else {
+        userAge.style.border = "2px solid green"}
+})
 
 
 selectForm.addEventListener("click", () =>{
@@ -22,7 +32,7 @@ selectForm.addEventListener("click", () =>{
         attentionString.style.display = "block";
     } else {
         modalWindow.style.display = "block"
-        modalWindow.innerHTML = 
+        modalWindow.innerHTML += 
         `<p>
         Ваше ФИО: ${userName.value}<br/>
         Вы родились ${date.value}<br/>
@@ -32,4 +42,7 @@ selectForm.addEventListener("click", () =>{
     }
 });
 
+mnemonic.addEventListener("click", () =>{
+    modalWindow.style.display = "none"
+})
 
